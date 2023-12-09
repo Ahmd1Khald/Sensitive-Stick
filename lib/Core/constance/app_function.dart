39698b471e60d17e54ea4fd36245c81f3acc119f
Ahmd1Khald <ahmed.khaled.sayed2000@gmail.com>
2023-12-09 03:32:12 +0200

    refactor code

--- lib/Core/constance/app_function.dart
+++ lib/Core/constance/app_function.dart
@@ -1,24 +1,7 @@
-import 'package:flutter/materiallors.dart';
+import 'package:flutter/material.dart';
 
 class AppFunctions {
   static void pushTo({required context, required Widget screen}) {
     Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
-    // Navigator.of(context).push(
-    //   PageRouteBuilder(
-    //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-    //       return SharedAxisTransition(
-    //         transitionType: SharedAxisTransitionType.horizontal,
-    //         secondaryAnimation: secondaryAnimation,
-    //         animation: animation,
-    //         child: child,
-    //       );
-    //     },
-    //     transitionDuration: const Duration(seconds: 1),
-    //     barrierColor: Colors.black,
-    //     pageBuilder: (context, animation, secondaryAnimation) {
-    //       return screen;
-    //     },
-    //   ),
-    // );
   }
 }
